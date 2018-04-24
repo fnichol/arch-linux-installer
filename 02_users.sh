7549f4074d26f23e64c0d82d37a13b7d5d78f9ae
Fletcher Nichol <fnichol@nichol.ca>
2018-04-23 18:58:45 -0600

    [02_users.sh] Update style.
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- 02_users.sh
+++ 02_users.sh
@@ -1,12 +1,80 @@
 #!/usr/bin/env bash
-set -eu
 
-if [ -n "${DEBUG:-}" ]; then
-  set -x
-fi
+main() {
+  set -eu
+  if [ -n "${DEBUG:-}" ]; then set -x; fi
+
+  version='0.1.0'
+  author='Fletcher Nichol <fnichol@nichol.ca>'
+  program="$(basename "$0")"
+
+  # ## CLI Argument Parsing
+
+  # Parse command line flags and options.
+  while getopts "Vh" opt; do
+    case $opt in
+      V)
+        echo "$program $version"
+        exit 0
+        ;;
+      h)
+        print_help
+        exit 0
+        ;;
+      \?)
+        print_help
+        exit_with "Invalid option: -$OPTARG" 1
+        ;;
+    esac
+  done
+  # Shift off all parsed token in `$*` so that the subcommand is now `$1`.
+  shift "$((OPTIND - 1))"
+
+  if [ -z "${1:-}" ]; then
+    print_help
+    exit_with "Required argument: <USERNAME>" 2
+  fi
+  admin="$1"
+  shift
+
+  if [ -z "${1:-}" ]; then
+    print_help
+    exit_with "Required argument: <FULLNAME>" 2
+  fi
+  admin_comment="$1"
+  shift
+
+  info "Installing OpenSSH and sudo"
+  pacman -S --noconfirm openssh sudo
+
+  info "Setting sudoers policy"
+  echo "%wheel ALL=(ALL) ALL" > /etc/sudoers.d/01_wheel
+
+  rm -f /etc/skel/.bashrc
+
+  info "Creating $admin user"
+  zfs create "tank/home/$admin"
+  sleep 2
+  useradd -m -G wheel -s /bin/bash -b /tmp -c "$admin_comment" "$admin"
+  chown -R "${admin}:${admin}" "/home/$admin"
+  chmod 0750  "/home/$admin"
+  (cd "/tmp/$admin"; tar cpf - . | tar xpf - -C "/home/$admin")
+  usermod -d "/home/$admin" "$admin"
+  rm -rf "/tmp/$admin"
+
+  info "Set root password"
+  passwd
+
+  info "Set $admin password"
+  passwd "$admin"
+
+  info "Starting OpenSSH service"
+  systemctl start sshd.socket
+  systemctl enable sshd.socket
+}
 
 print_help() {
-  printf -- "$program $version
+  echo "$program $version
 
 $author
 
@@ -22,112 +90,31 @@ COMMON FLAGS:
 ARGS:
     <USERNAME>    Admin username (ex: \`jdoe')
     <FULLNAME>    Admin name (ex: \`Jane Doe')
-
 "
 }
 
 info() {
   case "${TERM:-}" in
     *term | xterm-* | rxvt | screen | screen-*)
-      printf -- "   \033[1;36m${program:-unknown}: \033[1;37m${1:-}\033[0m\n"
+      printf -- "   \\033[1;36m%s: \\033[1;37m%s\\033[0m\\n" "${program}" "${1:-}"
       ;;
     *)
-      printf -- "   ${program:-unknown}: ${1:-}\n"
+      printf -- "   %s: %s\\n" "${program}" "${1:-}"
       ;;
   esac
   return 0
 }
 
 exit_with() {
-  if [ -n "${DEBUG:-}" ]; then set -x; fi
-
   case "${TERM:-}" in
     *term | xterm-* | rxvt | screen | screen-*)
-      echo -e "\033[1;31mERROR: \033[1;37m$1\033[0m"
+      printf -- "\\033[1;31mERROR: \\033[1;37m%s\\033[0m\\n" "${1:-}"
       ;;
     *)
-      echo "ERROR: $1"
+      printf -- "ERROR: %s" "${1:-}"
       ;;
   esac
-  exit ${2:-99}
+  exit "${2:-99}"
 }
 
-main() {
-  info "Installing OpenSSH and sudo"
-  pacman -S --noconfirm openssh sudo
-
-  info "Setting sudoers policy"
-  echo "%wheel ALL=(ALL) ALL" > /etc/sudoers.d/01_wheel
-
-  rm -f /etc/skel/.bashrc
-
-  info "Creating $admin user"
-  zfs create "tank/home/$admin"
-  sleep 2
-  useradd -m -G wheel -s /bin/bash -b /tmp -c "$admin_comment" "$admin"
-  chown -R "${admin}:${admin}" "/home/$admin"
-  chmod 0750  "/home/$admin"
-  (cd "/tmp/$admin"; tar cpf - . | tar xpf - -C /home/$admin)
-  usermod -d "/home/$admin" "$admin"
-  rm -rf "/tmp/$admin"
-
-  info "Set root password"
-  passwd
-
-  info "Set $admin password"
-  passwd "$admin"
-
-  info "Starting OpenSSH service"
-  systemctl start sshd.socket
-  systemctl enable sshd.socket
-}
-
-
-# # Main Flow
-
-# The current version of this program
-version='0.1.0'
-# The author of this program
-author='Fletcher Nichol <fnichol@nichol.ca>'
-# The short version of the program name which is used in logging output
-program="$(basename $0)"
-
-
-# ## CLI Argument Parsing
-
-# Parse command line flags and options.
-while getopts "Vh" opt; do
-  case $opt in
-    V)
-      echo "$program $version"
-      exit 0
-      ;;
-    h)
-      print_help
-      exit 0
-      ;;
-    \?)
-      print_help
-      exit_with "Invalid option: -$OPTARG" 1
-      ;;
-  esac
-done
-# Shift off all parsed token in `$*` so that the subcommand is now `$1`.
-shift "$((OPTIND - 1))"
-
-if [ -z "${1:-}" ]; then
-  print_help
-  exit_with "Required argument: <USERNAME>" 2
-fi
-admin="$1"
-shift
-
-if [ -z "${1:-}" ]; then
-  print_help
-  exit_with "Required argument: <FULLNAME>" 2
-fi
-admin_comment="$1"
-shift
-
-main
-exit 0
+main "$@" || exit 99
